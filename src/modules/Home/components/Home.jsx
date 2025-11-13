113eb8059fafffbc670baff11ea830045692074e
Soumyodipto68 <soumyodiptopal77@gmail.com>
2025-11-13 21:42:12 +0530

    Anoter Commit1

--- src/modules/Home/components/Home.jsx
+++ src/modules/Home/components/Home.jsx
@@ -7,7 +7,6 @@ import Footer from '../../../components/Footer/Footer'
 
 // Home component that includes Carousal, Feature, CTA, and Footer components
 // Additional comments can be added here if necessary
-// Additional comments can be added here if necessary
 
 const Home = () => {
   return (
